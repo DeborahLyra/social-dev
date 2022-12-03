a8b595f954fbc853002e059a1b62d95e92426935
DeborahLyra <deborahp.lyra@gmail.com>
2022-12-03 17:49:03 -0300

    Criado esqueleto da página de login

--- src/components/layout/ImageWithSpace.jsx
+++ src/components/layout/ImageWithSpace.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 
-const Width_Break = '600px'
+const Width_Break = '700px'
 
 const StyledFlex = styled.div`
     display: flex;
@@ -21,14 +21,18 @@ const StyledImage = styled.div`
 
 const StyledContainer = styled.div`
     background-color: white;
-    padding: 30px;
+    padding: 30px 50px;
 
     @media (min-width: ${Width_Break}){
-        min-width: calc (${Width_Break} - 60px); 
+      width: calc (${Width_Break} - 100px); 
     }
     @media (max-width: ${Width_Break}){
         width: 100%;
     }
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 `
 
 
